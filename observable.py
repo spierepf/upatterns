e89f9cd106bd60a5c60892e32919311f613e5c0b
Peter-Frank Spierenburg <spierepf@hotmail.com>
2025-07-06 10:37:50 -0300

    Update observable.py
    privatized callbacks

--- observable.py
+++ observable.py
@@ -1,13 +1,13 @@
 class Observable:
     def __init__(self):
-        self.callbacks = []
+        self._callbacks = []
 
     def detach(self, callback):
-        self.callbacks.remove(callback)
+        self._callbacks.remove(callback)
 
     def attach(self, callback):
-        self.callbacks.append(callback)
+        self._callbacks.append(callback)
 
     def notify(self, *args, **kwargs):
-        for callback in list(self.callbacks):
+        for callback in list(self._callbacks):
             callback(*args, **kwargs)
